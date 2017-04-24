f0d2ba5b3aecf81fb95439bbeb8e9de7924ff02a
Italo Silva <italo.ecomp@gmail.com>
2017-04-23 23:13:19 -0300

    Calculo da taxa de acerto do algoritmo

--- classificacao.py
+++ classificacao.py
@@ -21,7 +21,7 @@ from sklearn.naive_bayes import MultinomialNB
 # Criar um modelo seguindo o naive_bayes
 modelo = MultinomialNB()
 
-# Realiza o treino do adequa o modelo
+# Realiza o treino do algoritmo adequa o modelo
 modelo.fit(dados, marcacoes)
 
 # Dados que qeremos classificar
@@ -30,8 +30,20 @@ misterioso2 = [1, 0, 0]
 misterioso3 = [0, 0, 1]
 
 teste = [misterioso1, misterioso2, misterioso3]
+marcacoes_teste = [-1, 1, 1]
 
-# Realizamos a identificacao dos animais "Predição"
+# Realizamos a identificacao dos animais "Predicao"
 resultado = modelo.predict(teste)
 
+diferencas = resultado - marcacoes_teste
+
+acertos = [d for d in diferencas if d == 0]
+
+total_de_elementos = len(teste)
+numero_de_acertos = len(acertos)
+
+taxa_de_acerto = 100.0 * numero_de_acertos / total_de_elementos
+
+print(diferencas)
 print(resultado)
+print(taxa_de_acerto)
