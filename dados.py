c979412ae4e14cbec2bbdbe1f43256fbd44e636b
Italo Silva <italo.ecomp@gmail.com>
2017-04-23 23:42:27 -0300

    Implementacao da Abertura de arquivo para a classificao

---
+++ dados.py
@@ -0,0 +1,17 @@
+import csv
+
+def carregar_acessos():
+	
+	dados = []
+	marcacoes = []
+
+	arquivo = open('acessos.csv', 'rb')
+	leitor = csv.reader(arquivo)
+
+	for acessou_home, acessou_como_funciona,acessou_contato, comprou in leitor:
+		
+		dados.append([acessou_home, acessou_como_funciona, acessou_contato])
+
+		marcacoes.append(comprou)
+
+	return dados, marcacoes
